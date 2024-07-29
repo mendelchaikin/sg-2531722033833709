9630203ef1053b17f40968148f15c8a08458ea72
SoftGen <admin@softgen.ai>
2024-07-29 05:05:58 +0000

    SOFTGEN MAGIC

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,27 @@
 import { useEffect } from 'react';
+import Layout from '@/components/Layout';
+import WelcomeSection from '@/components/WelcomeSection';
+import PopularGames from '@/components/PopularGames';
+import Categories from '@/components/Categories';
+import { useGameContext } from '@/context/GameContext';
+import { sampleGames } from '@/data/sampleGames';
 
 const Home = () => {
+  const { setGames } = useGameContext();
+
   useEffect(() => {
+    setGames(sampleGames);
     console.log('Home component mounted - Client side log');
-  }, []);
+  }, [setGames]);
 
   console.log('Home component rendering - This should appear in server logs');
 
   return (
-    <div>
-      <h1>Welcome to the Gaming Site - Test Version</h1>
-      <p>If you can see this, the page is working.</p>
-    </div>
+    <Layout>
+      <WelcomeSection />
+      <PopularGames games={sampleGames} />
+      <Categories categories={[...new Set(sampleGames.map(game => game.category))]} />
+    </Layout>
   );
 };
 
